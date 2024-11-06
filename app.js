e2b6d64f2cf79fc5f62be01625e362e0fe10d169
gabimoreira1 <160953261+gabimoreira1@users.noreply.github.com>
2024-11-06 17:26:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function criarCartao(categoria,pergunta,resposta){
+    let container = document.grtElementById('container')
+    let cartao = document.createElement('article')
+    cartao,className = 'cartao'
+}
+cartao.innerHTML =
+<div class="cartão__conteudo"></div>
+<h3>programação</h3>
+<div class="cartão__conteudo__pergunta">
+   <p>O  que é JavaScript?</p> 
+</div>
+<div class="cartão__conteudo__resposta">
+    <p>O JavaScript é uma linguagem deprogramação</p>
+</div>
+</div>
