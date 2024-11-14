1b94873fdbcb3862e78515f208123b9d846338fb
gabimoreira1 <160953261+gabimoreira1@users.noreply.github.com>
2024-11-14 17:16:15 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,28 @@
-criaCartao {
-  pergunta,
-  categoria,
-  resposta,
-}
-criaCartao{
-  'Programação',
-  'O que é uma linguagem de programação',
-  'O javaScript é linguagem de programação'
-}
-criaCartao{
-  'Geografia',
-  'Qual a capital da França?',
-  'A capital da França é Paris'
-}
-criaCartao{
-  'Programação',
-  'O que é uma função',
-  'Uma função é um bloco de código que executa alguma tarefa'
-}
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
